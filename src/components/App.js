f8affc2443d4d514f664885039e1940b38e1c35c
Jayprakash Sahu <118419073+Jpfpp12@users.noreply.github.com>
2025-05-05 11:45:52 +0000

    {commit_message}

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import UserList from "./UserList";
-import UserDetails from "./UserDetails";
+import UserList from "./UserList.js";
+import UserDetails from "./UserDetails.js";
 
 const App = () => {
   return (
